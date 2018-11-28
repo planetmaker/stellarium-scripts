578daa9345a8024c20bc56d2d4142a59dadd1ebd
Ingo von Borstel <planetmaker@openttd.org>
2018-11-28 21:50:38 +0100

    Codechange: Move setting changes to its own subroutine

--- stellarium_reports.py
+++ stellarium_reports.py
@@ -28,7 +28,7 @@ def start_stellarium(url, screenshot_dir):
         requests.get(url + "main/status")
     except:
         proc_stellarium = subprocess.Popen(['stellarium','--screenshot-dir', screenshot_dir], stdout=subprocess.PIPE);
-        sleep(5)
+        sleep(10)
     else:
         print("Found running stellarium. We are using it.")
         
@@ -52,67 +52,71 @@ def make_screenshot_dir():
             
     return screenshot_dir
 
+def setup_viewport(properties):
+    param_fov = {'fov':'190'}
+    url_fov = "main/fov"
+    fov = requests.post(url_main+url_fov, data=param_fov)
+    print("FOV: ",fov)
+    param_view = {'altAz':'[0.0001,0,1]'}
+    url_view = "main/view"
+    view = requests.post(url_main+url_view, data=param_view)
+    print("View: ",view)
+    
+    settings = [
+        {'url':'stelproperty/set', 'id':'actionShow_Nebulas', 'value':False},
+        {'url':'stelproperty/set', 'id':'SolarSystem.planetsDisplayed', 'value': False},
+        {'url':'stelproperty/set', 'id':'actionShow_MeteorShowers', 'value': False},
+        {'url':'stelproperty/set', 'id':'actionShow_Planets', 'value': False},
+        {'url':'stelproperty/set', 'id':'actionShow_DSO_Textures', 'value': False},
+        ]
+    for item in settings:
+        answer = requests.post(url_main + item.get('url'), data={'id':item.get('id'), 'value':item.get('value')})
+        print("{}: {}".format(item.get('id'),answer.text))
+
+def make_screenshot():
+    """ Create a screenshot of the given name)
+    """
+    url_screenshot = 'stelaction/do'
+    screenshot = requests.post(url_main + url_screenshot, data={'id':'actionSave_Screenshot_Global'})
+    print("Screenshot: ",screenshot)
 
 
 url_main = "http://localhost:8090/api/"
+screenshot_dir = ""
 
-screenshot_dir = make_screenshot_dir()
-proc_stellarium = start_stellarium(url_main, screenshot_dir)
+def main(url_main):
+    screenshot_dir = make_screenshot_dir()
+    proc_stellarium = start_stellarium(url_main, screenshot_dir)
+    
+    url_status = "main/status"
+    url_actionlist = "stelaction/list"
+    url_properties = "stelproperty/list"
+    response = requests.get(url_main + url_status)
+    print("Status: ",response.status_code)
+    actions = requests.get(url_main + url_actionlist)
+    print("Actions: ",actions.status_code)
+    properties = requests.get(url_main + url_properties)
+    print("Properties: ",properties.status_code)
+    
+    #print(response.json())
+    #print(actions.json())
+    #print(properties.json())
+    
+    #jd_next = response.json().get('time').get('jday') + 60
+    #url_time = "main/time"
+    #param_main = {'time':str(jd_next)}
+    #answer = requests.post(url_main+url_time, data=param_main)
+    #print("Set time: ",answer.status_code)
+    
+    setup_viewport(properties.json())
+    # Sleep so that the commands have been executed
+    sleep(5)
+    
+    make_screenshot()
+    
+    if proc_stellarium is not None:
+        print("We started it, so we are closing down stellarium now")
+        proc_stellarium.kill()
 
-url_status = "main/status"
-url_actionlist = "stelaction/list"
-url_properties = "stelproperty/list"
-response = requests.get(url_main + url_status)
-print("Status: ",response.status_code)
-actions = requests.get(url_main + url_actionlist)
-print("Actions: ",actions.status_code)
-properties = requests.get(url_main + url_properties)
-print("Properties: ",properties.status_code)
-
-#print(response.json())
-#print(actions.json())
-#print(properties.json())
-
-#if response.status_code == 200:
-#    print("Time: ", response.json().get('time'))
-#    print("Location: ", response.json().get('location'))
-#    print("View: ", response.json().get('view'))
-#
-
-#jd_next = response.json().get('time').get('jday') + 60
-#url_time = "main/time"
-#param_main = {'time':str(jd_next)}
-#answer = requests.post(url_main+url_time, data=param_main)
-#print("Set time: ",answer.status_code)
-
-#sleep(1)
-
-param_fov = {'fov':'190'}
-url_fov = "main/fov"
-fov = requests.post(url_main+url_fov, data=param_fov)
-print("FOV: ",fov)
-param_view = {'altAz':'[0.0001,0,1]'}
-url_view = "main/view"
-view = requests.post(url_main+url_view, data=param_view)
-print("View: ",view)
-#if answer.status_code == 200:
-#    print(answer.json())
-#else:
-#    print("Post request did not succeed")
-
-#if actions.status_code == 200:
-#    print(actions.json())
-
-sleep(1)
-
-url_screenshot = 'stelaction/do'
-screenshot = requests.post(url_main + url_screenshot, data={'id':'actionSave_Screenshot_Global'})
-print("Screenshot: ",screenshot)
-
-url_planets = 'stelaction/do'
-planet = requests.post(url_main + url_planets, data={'id':'actionShow_Planets'})
-print("Show planets: ",planet)
-
-if proc_stellarium is not None:
-    print("We started it, so we are closing down stellarium now")
-    proc_stellarium.kill()
+if __name__ == "__main__":
+    main(url_main)
